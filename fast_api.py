7c8ec09a91e68c3da9c549278e65f97545256ad3
Le Viet Thanh <115569080+samuel7232003@users.noreply.github.com>
2024-12-19 10:05:05 +0700

    [update] 2

--- fast_api.py
+++ fast_api.py
@@ -80,4 +80,4 @@ def predict():
         return jsonify({"error": f"An error occurred: {e}"}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='127.0.0.1', port=8000)
